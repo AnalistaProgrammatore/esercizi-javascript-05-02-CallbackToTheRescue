194939983ecff72220a6c5247687c2f43900b78e
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-07 12:39:33 +0100

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -4,4 +4,15 @@
 * The characters should form a chessboard.
 */
 
-
+let a = ''
+for (b = 0; b < 4; b++) {
+    for (let x = 0; x < 8; x++) {
+        x % 2 === 0 ? a += '#' : a += ' '
+    }
+    a += '\n'
+    for (let y = 0; y < 8; y++) {
+        y % 2 === 0 ? a += ' ' : a += '#'
+    }
+    a += '\n'
+}
+console.log(a)
