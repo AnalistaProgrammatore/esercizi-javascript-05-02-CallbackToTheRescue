2874f8b29c5f0ef329bd4067bbf216169d71fe06
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-07 11:16:06 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,3 +4,13 @@
 * When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
 * (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
+
+for (let i=0; i<100; i++){
+  let a= ''
+  i % 15 === 0 ? a+='FizzBuzz'
+: i % 5 === 0 ? a+='Buzz' 
+: i % 3 === 0 ? a+='Fizz' : a+=i
+  console.log(a)
+}
+
+//purtroppo non sono riuscito a trovare un modo per evitare di fare uscire FizzBuzz al primo giro del ciclo.
