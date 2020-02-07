eb7957ec46cc582d93de01ca9108fa4a81945077
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-07 12:13:55 +0100

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -4,13 +4,4 @@
 * The characters should form a chessboard.
 */
 
-let a = ' '
-for(let i = 0; i < 8; i++){
-  for( let x = 0; x < 8; x++){
-     x % 2 === 0 ? a+='#' : a+=' ' 
-  }
-  a += '\n'
-}
-console.log(a)
 
-//non sono riuscito a fare in modo che la riga successiva restituisca i caratteri invertiti rispetto a quella precedente
