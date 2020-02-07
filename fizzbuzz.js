ba7e1bc8617aa1cff8624398adb09c6e16666b2e
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-07 18:29:16 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,12 +4,3 @@
 * When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
 * (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
-for (let i = 0; i < 100; i++) {
-    let a = 0
-    if (i > 0) {
-        i % 15 === 0 ? a += 'FizzBuzz'
-            : i % 5 === 0 ? a += 'Buzz'
-                : i % 3 === 0 ? a += 'Fizz' : a += i
-        console.log(a)
-    }
-}
